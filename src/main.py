c1fb50bd40051307fcd5825b8ef8a27ebfa7583f
kuvuś <kuba17ism@tuta.io>
2023-12-12 17:00:00 +0100

    Add GUI draft

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 from .wolf_simulation import Simulation
 from .agent import Agent
 from .wolf import Wolf
@@ -8,7 +9,7 @@ from .deer import Deer
 pygame.init()
 
 # Window settings
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1000, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Grid and camera settings
@@ -52,6 +53,9 @@ def process_events():
                 print("run/pause")
             if event.key == pygame.K_TAB:
                 simulation.step()
+        else:
+            ui_events(event)
+        manager.process_events(event)
 
 def draw_grid(surface: pygame.Surface, simulation: Simulation):
     for x in range(simulation.width):
@@ -85,7 +89,47 @@ def draw_world(simulation) -> pygame.Surface:
     return surface
 
 def draw_ui() -> pygame.Surface:
-    raise NotImplementedError
+    manager = pygame_gui.UIManager((800, 600))
+    next_step_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 10), (200, 50)),
+                                                text='Next step',
+                                                manager=manager)
+    run_sim_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 70), (200, 50)),
+                                                text='Run simulation',
+                                                manager=manager)
+    reset_sim_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 130), (200, 50)),
+                                                text='Reset simulation',
+                                                manager=manager)
+    pygame_gui.elements.UITextBox(
+        html_text="Steps per second",
+        relative_rect=pygame.Rect(10, 190, 200, 30),
+        manager=manager)
+    steps_ps_slider = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((10, 220), (200, 50))
+                                                             ,start_value=0.5, value_range=(0, 10),
+                                                             manager=manager)
+
+    elements = {
+        "next_step_btn": next_step_btn,
+        "steps_ps_slider": steps_ps_slider,
+        "run_sim_btn": run_sim_btn,
+        "reset_sim_btn": reset_sim_btn
+    }
+    return manager, elements
+
+def ui_events(event):
+    global ui_elements
+    if event.type == pygame_gui.UI_BUTTON_PRESSED:
+        if event.ui_element == ui_elements["next_step_btn"]:
+            simulation.step()
+        elif event.ui_element == ui_elements["run_sim_btn"]:
+            print('run sim button pressed!')
+        elif event.ui_element == ui_elements["reset_sim_btn"]:
+            print('reset sim button pressed!')
+    elif event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
+        if event.ui_element == ui_elements["steps_ps_slider"]:
+            print(event.value)
+
+
+manager, ui_elements = draw_ui()
 
 def main():
     clock = pygame.time.Clock()
@@ -94,15 +138,17 @@ def main():
         process_events()
         move_camera()
 
+        time_delta = clock.tick(60)/1000.0
+        manager.update(time_delta)
+
         # if should perform step then update the simulation
 
         world_surface = draw_world(simulation)
         scaled_surface = pygame.transform.scale(world_surface, (int(WIDTH * zoom_level), int(HEIGHT * zoom_level)))
         window.fill(WHITE)
         window.blit(scaled_surface, (-camera_x, -camera_y)) # Draw surface while applying camera translation
-
+        manager.draw_ui(window)
         pygame.display.flip()
-        clock.tick(60)
 
     pygame.quit()
 
