f962c1e950e713a036be8215a7754a0b26cac959
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-12-13 00:29:53 +0100

    better colors

--- src/drawing.py
+++ src/drawing.py
@@ -2,7 +2,8 @@ import pygame
 from .wolf_simulation import Simulation
 
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+GRASS_COLOR = (0, 160, 0)
+GRID_COLOR = (0, 0, 0)
 CELL_SIZE_PX = 40
 
 def draw_ground(simulation: Simulation) -> pygame.Surface:
@@ -11,8 +12,8 @@ def draw_ground(simulation: Simulation) -> pygame.Surface:
     for x in range(simulation.width):
         for y in range(simulation.height):
             rect = pygame.Rect(x * CELL_SIZE_PX, y * CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX)
-            pygame.draw.rect(surface, (0, 255, 0), rect)
-            pygame.draw.rect(surface, BLACK, rect, 1)
+            pygame.draw.rect(surface, GRASS_COLOR, rect)
+            pygame.draw.rect(surface, GRID_COLOR, rect, 1)
     return surface
 
 def draw_agents(simulation: Simulation) -> pygame.Surface:
@@ -24,10 +25,10 @@ def draw_agents(simulation: Simulation) -> pygame.Surface:
                 pygame.draw.rect(surface, (255, 0, 0), rect)
             case "wolf":
                 rect = pygame.Rect(agent.x * CELL_SIZE_PX, agent.y * CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX)
-                pygame.draw.rect(surface, (120, 120, 120), rect)
+                pygame.draw.rect(surface, (100, 100, 100), rect)
             case "deer":
                 rect = pygame.Rect(agent.x * CELL_SIZE_PX, agent.y * CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX)
-                pygame.draw.rect(surface, (255, 255, 0), rect)
+                pygame.draw.rect(surface, (205, 133, 63), rect)
             case _:
                 raise NotImplementedError(f"can't display agent of kind {agent.kind}")
     return surface
