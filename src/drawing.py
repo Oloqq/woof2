0d540ac89059fcb8ef21bff96a1f64dbd56d6973
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-12-13 03:44:33 +0100

    water drawing

--- src/drawing.py
+++ src/drawing.py
@@ -1,8 +1,9 @@
 import pygame
-from .wolf_simulation import Simulation
+from .wolf_simulation import Simulation, Terrain
 
 WHITE = (255, 255, 255)
 GRASS_COLOR = (0, 160, 0)
+WATER_COLOR = (0, 0, 160)
 GRID_COLOR = (0, 0, 0)
 CELL_SIZE_PX = 40
 
@@ -12,7 +13,11 @@ def draw_ground(simulation: Simulation) -> pygame.Surface:
     for x in range(simulation.width):
         for y in range(simulation.height):
             rect = pygame.Rect(x * CELL_SIZE_PX, y * CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX)
-            pygame.draw.rect(surface, GRASS_COLOR, rect)
+            match simulation.grid[x][y].terrain:
+                case Terrain.Grass:
+                    pygame.draw.rect(surface, GRASS_COLOR, rect)
+                case Terrain.Water:
+                    pygame.draw.rect(surface, WATER_COLOR, rect)
             pygame.draw.rect(surface, GRID_COLOR, rect, 1)
     return surface
 
--- src/wolf_simulation.py
+++ src/wolf_simulation.py
@@ -1,12 +1,28 @@
 from .agent import Agent
+from enum import Enum, auto
+
+class Terrain(Enum):
+    Grass = auto(),
+    Water = auto()
+
+class Cell:
+    def __init__(self):
+        self.scent = 0
+        self.terrain = Terrain.Grass
 
 class Simulation:
     def __init__(self, world_size):
         self.width = world_size[0]
         self.height = world_size[1]
         self.agents: list[Agent] = []
-        self.scent = [[0 for _ in range(self.height)] for _ in range(self.width)]
         self.deathnote: list[Agent] = []
+        self.grid: list[list[Cell]] = [[Cell() for _ in range(self.height)] for _ in range(self.width)]
+        for x in range(self.width):
+            self.grid[x][0].terrain = Terrain.Water
+            self.grid[x][self.height-1].terrain = Terrain.Water
+        for y in range(self.height):
+            self.grid[0][y].terrain = Terrain.Water
+            self.grid[self.width-1][y].terrain = Terrain.Water
         self.reset()
 
     def reset(self):
