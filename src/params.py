e7625063874187aa940048de3cc1534f8bfe5ebd
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-12-16 19:08:48 +0100

    smol grid

--- src/params.py
+++ src/params.py
@@ -1,7 +1,7 @@
 WINDOW_SIZE = (1000, 600) # outside Params class because it will never be settable via a config file
 
 class Params:
-    grid_size: tuple[int, int] = (2000, 2000)
+    grid_size: tuple[int, int] = (100, 100)
 
     deer_herd_territory_length = 8
     deer_herd_size: int = 20
